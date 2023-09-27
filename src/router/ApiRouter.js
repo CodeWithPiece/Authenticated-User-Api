fcc5ff269df4b677401fc92d4c98b61050773273
Nirmal <ignirmal12@gmail.com>
2023-09-27 13:55:49 +0530

    auth validation middleware

--- src/router/ApiRouter.js
+++ src/router/ApiRouter.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
+const authValidate = require("../../helper/AuthValidate");
 const UserController = require("../controller/UserController");
 router.post("/user/create", UserController.saveUser);
-router.get("/users", UserController.getUsers);
+router.get("/users", authValidate.isAuthorize, UserController.getUsers);
 router.get("/mail-verification", UserController.verifyUser);
 router.post("/login", UserController.doLogin);
 
